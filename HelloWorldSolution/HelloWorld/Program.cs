807b4d659822bbceb60d21967ee27d9d7f8a1e10
Amy Lyon <amy_lyon@progressive.com>
2022-12-05 15:23:36 -0500

    break timer

---
+++ HelloWorldSolution/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using HelloWorld;
+
+Console.WriteLine("Take a break!");
+
+Console.Write("How many mins? :");
+string? minutes = Console.ReadLine();
+
+if (minutes is not null)
+{
+    DateUtils utils = new DateUtils();
+    int mins = int.Parse(minutes);
+    DateTime timeAtEndOfBreak = utils.TakeABreak(mins);
+    Console.WriteLine($"Okay, be back at {timeAtEndOfBreak:T}");
+}
+else
+{
+    Console.WriteLine("Enter some mins");
+}
